066e05664b35f3500f1286585ba3e3e7421ee8f6
IFEANYI OMEATA <omeatai@gmail.com>
2020-10-16 10:24:37 +0100

    init

--- datatypes.py
+++ datatypes.py
@@ -11,8 +11,75 @@ print(type(x))
 #print(x)
 #print(type(x)) 
 
-x = 20
-x = int(20)
-print(x)
-print(type(x)) 
+# x = 20.5
+# x = float(20.5)
+# print(x)
+# print(type(x)) 
+
+# x = 1j
+# x = complex(1j)
+# print(x)
+# print(type(x)) 
+
+# x = ["apple", "banana", "cherry"]
+# x = list(("apple", "banana", "cherry"))
+# print(x)
+# print(type(x)) 
+
+# x = ("apple", "banana", "cherry")
+# x = tuple(("apple", "banana", "cherry"))
+# print(x)
+# print(type(x)) 
+
+# x = range(6)
+# print(x)
+# print(type(x)) 
+
+# x = {"name" : "John", "age" : 36}
+# x = dict(name="John", age=36)
+# print(x)
+# print(type(x)) 
+
+# x = {"apple", "banana", "cherry"}
+# x = set(("apple", "banana", "cherry"))
+# print(x)
+# print(type(x)) 
+
+# x = frozenset({"apple", "banana", "cherry"})
+# x = frozenset(("apple", "banana", "cherry"))
+# print(x)
+# print(type(x)) 
+
+# x = True
+# x = bool(5)
+# print(x)
+# print(type(x)) 
+
+# x = b"Hello“
+# x = bytes(5)
+# print(x)
+# print(type(x)) 
+
+# x = bytearray(5)
+# print(x)
+# print(type(x)) 
+
+# x = memoryview(bytes(5))
+# print(x)
+# print(type(x)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
