20b22cec6fdf1c48285508a579d260987f292d3b
IFEANYI OMEATA <omeatai@gmail.com>
2020-10-16 11:20:12 +0100

    init

--- datatypes.py
+++ datatypes.py
@@ -1,69 +1,82 @@
-x = 7
-print(type(x))
+#x = 7
+#print(type(x))
 
-#x = "Hello World"
-#x = str("Hello World")
-#print(x)
-#print(type(x)) 
+#This is the way to set the string datatype:
+# x = "Hello World"
+# x = str("Hello World")
+# print(x)
+# print(type(x)) 
 
-#x = 20
-#x = int(20)
-#print(x)
-#print(type(x)) 
+# This is the way to set the int datatype:
+# x = 20
+# x = int(20)
+# print(x)
+# print(type(x)) 
 
+#This is the way to set the float datatype:
 # x = 20.5
 # x = float(20.5)
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the complex datatype:
 # x = 1j
 # x = complex(1j)
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the list datatype:
 # x = ["apple", "banana", "cherry"]
 # x = list(("apple", "banana", "cherry"))
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the tuple datatype:
 # x = ("apple", "banana", "cherry")
 # x = tuple(("apple", "banana", "cherry"))
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the range datatype:
 # x = range(6)
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the dictionary datatype:
 # x = {"name" : "John", "age" : 36}
 # x = dict(name="John", age=36)
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the set datatype:
 # x = {"apple", "banana", "cherry"}
 # x = set(("apple", "banana", "cherry"))
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the frozenset datatype:
 # x = frozenset({"apple", "banana", "cherry"})
 # x = frozenset(("apple", "banana", "cherry"))
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the boolean datatype:
 # x = True
 # x = bool(5)
 # print(x)
 # print(type(x)) 
 
-# x = b"Hello“
+#This is the way to set the bytes datatype:
 # x = bytes(5)
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the bytearray datatype:
 # x = bytearray(5)
 # print(x)
 # print(type(x)) 
 
+#This is the way to set the memoryview datatype:
 # x = memoryview(bytes(5))
 # print(x)
 # print(type(x)) 
