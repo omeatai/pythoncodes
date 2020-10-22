0397193766b462a53248da30cd723a0bc7e27df6
IFEANYI OMEATA <omeatai@gmail.com>
2020-10-22 22:55:23 +0100

    init

---
+++ type_casting.py
@@ -0,0 +1,32 @@
+# y = 1
+# x = 2 + 3j;
+# z = 2.0e10 + 3.0j;
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+# #convert from int to float:
+# a = float(x)
+# #convert from float to int:
+# b = int(y)
+# #convert from int to complex:
+# c = complex(x)
+# print(a)
+# print(b)
+# print(c)
+# print(type(a))
+# print(type(b))
+# print(type(c)) 
+
+# x = str("4men")
+# y = str(2)
+# z = str(3.0)
+# print(x)
+# print(y)
+# print(z)
+# print(type(x))
+# print(type(y))
+# print(type(z)) 
